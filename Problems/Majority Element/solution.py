338bf8cd94fc8361abc4d5503feebc02827d5286
Renan Santana <renan_ota@hotmail.com>
2024-01-23 18:30:11 -0300

    Solved "Majority Element"

---
+++ Problems/Majority Element/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+
+        for i in nums:
+            if count == 0:
+                candidate = i
+
+            count += 1 if i == candidate else -1
+
+        return candidate
